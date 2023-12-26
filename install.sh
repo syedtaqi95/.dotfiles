a80b7fb0a4ab9346cc1466e84bd0ecede0a5495d
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-12-26 12:41:55 +0000

    Working install script

--- install.sh
+++ install.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
 
-# Link neovim config
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+create_symlink() {
+    local source=$1
+    local target=$2
+
+    if [ ! -f "$source" ]; then
+        echo "Source file does not exist: $source"
+        exit 1
+    fi
+
+    if [ -e "$target" ]; then
+        echo "Target already exists, backing up: $target.bak"
+        mv "$target" "${target}.bak"
+    fi
+
+    mkdir -p "$(dirname "$target")"
+    ln -nfs "$source" "$target"
+    echo "Symlink created: $target -> $source"
+
+    if [ -e "${target}.bak" ]; then
+        rm "${target}.bak"
+    fi
+
+}
+
+# Neovim
+echo "Setting up Neovim..."
+SOURCE="$SCRIPT_DIR/init.lua"
+TARGET="$HOME/.config/nvim/init.lua"
+create_symlink "$SOURCE" "$TARGET"
+
+# Tmux
+echo
+echo "Setting up tmux..."
+SOURCE="$SCRIPT_DIR/.tmux.conf"
+TARGET="$HOME/.tmux.conf"
+create_symlink "$SOURCE" "$TARGET"
+
