66509fd9a6bda32643b848fa253d98e293b908b8
syedtaqi95 <sh13226@my.bristol.ac.uk>
2024-03-06 12:28:17 +0000

    Added gx.nvim, updated lualine config

--- init.lua
+++ init.lua
@@ -175,6 +175,7 @@ require('lazy').setup({
       },
       sections = {
         lualine_c = { { 'filename', path = 1 } },
+        lualine_x = { 'encoding', 'filetype' },
       },
     },
   },
@@ -325,6 +326,18 @@ require('lazy').setup({
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     ft = { 'markdown' },
     build = function() vim.fn['mkdp#util#install']() end,
+  },
+
+  {
+    "chrishrb/gx.nvim",
+    keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+    cmd = { "Browse" },
+    init = function()
+      vim.g.netrw_nogx = 1 -- disable netrw gx
+    end,
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = true,      -- default settings
+    submodules = false, -- not needed, submodules are required only for tests
   },
   -- NOTE: The import below can automatically add your own plugins,
   -- configuration, etc from `lua/custom/plugins/*.lua`
