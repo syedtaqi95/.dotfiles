6696326dfb308ed1cd5b9f6b2cc9d972199e4d0b
syedtaqi95 <sh13226@my.bristol.ac.uk>
2024-09-30 11:01:28 +0100

    Added keymaps for git file history and git line history

--- init.lua
+++ init.lua
@@ -572,6 +572,11 @@ end
 vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
 vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+vim.keymap.set('n', '<leader>gh', ':G log %<CR>',  { desc = '[G]it File [H]istory' })
+vim.keymap.set('n', '<leader>gl', function()
+    vim.cmd('G log -L' .. vim.fn.line('.') .. ',' .. vim.fn.line('.') .. ':' .. '%')
+  end,
+  { desc = '[G]it [L]ine History' })
 vim.keymap.set('n', '<leader>sff', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sfh', function()
     require 'telescope.builtin'.find_files({ hidden = true })
