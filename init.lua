ae4cfcefb59f63dde759084b28adc89034e4866f
syedtaqi95 <sh13226@my.bristol.ac.uk>
2024-11-04 23:42:03 +0000

    fix: bufferline colours, tmux default-terminal

--- init.lua
+++ init.lua
@@ -228,67 +228,187 @@ require('lazy').setup({
     version = '*',
     dependencies = 'nvim-tree/nvim-web-devicons',
     opts = {
+      options = {
+        diagnostics = 'nvim_lsp',
+      },
       highlights = {
         fill = {
-          bg = 'none'
+          bg = 'none',
+        },
+        background = {
+          bg = 'none',
+        },
+        tab = {
+          bg = 'none',
         },
         tab_selected = {
-          bg = 'none'
+          bg = 'none',
+        },
+        tab_separator = {
+          bg = 'none',
         },
         tab_separator_selected = {
-          bg = 'none'
+          bg = 'none',
+        },
+        tab_close = {
+          bg = 'none',
+        },
+        close_button = {
+          bg = 'none',
+        },
+        close_button_visible = {
+          bg = 'none',
         },
         close_button_selected = {
-          bg = 'none'
+          bg = 'none',
+        },
+        buffer_visible = {
+          bg = 'none',
         },
         buffer_selected = {
-          bg = 'none'
+          bg = 'none',
+          bold = true,
+          italic = true,
+        },
+        numbers = {
+          bg = 'none',
+        },
+        numbers_visible = {
+          bg = 'none',
         },
         numbers_selected = {
-          bg = 'none'
+          bg = 'none',
+          bold = true,
+        },
+        diagnostic = {
+          bg = 'none',
+        },
+        diagnostic_visible = {
+          bg = 'none',
         },
         diagnostic_selected = {
-          bg = 'none'
+          bg = 'none',
+        },
+        hint = {
+          bg = 'none',
+        },
+        hint_visible = {
+          bg = 'none',
         },
         hint_selected = {
-          bg = 'none'
+          bg = 'none',
+        },
+        hint_diagnostic = {
+          bg = 'none',
+        },
+        hint_diagnostic_visible = {
+          bg = 'none',
         },
         hint_diagnostic_selected = {
-          bg = 'none'
+          bg = 'none',
+        },
+        info = {
+          bg = 'none',
+        },
+        info_visible = {
+          bg = 'none',
         },
         info_selected = {
-          bg = 'none'
+          bg = 'none',
+        },
+        info_diagnostic = {
+          bg = 'none',
+        },
+        info_diagnostic_visible = {
+          bg = 'none',
         },
         info_diagnostic_selected = {
-          bg = 'none'
+          bg = 'none',
+        },
+        warning = {
+          bg = 'none',
+        },
+        warning_visible = {
+          bg = 'none',
         },
         warning_selected = {
-          bg = 'none'
+          bg = 'none',
+        },
+        warning_diagnostic = {
+          bg = 'none',
+        },
+        warning_diagnostic_visible = {
+          bg = 'none',
         },
         warning_diagnostic_selected = {
-          bg = 'none'
+          bg = 'none',
+        },
+        error = {
+          bg = 'none',
+        },
+        error_visible = {
+          bg = 'none',
         },
         error_selected = {
-          bg = 'none'
+          bg = 'none',
+        },
+        error_diagnostic = {
+          bg = 'none',
+        },
+        error_diagnostic_visible = {
+          bg = 'none',
         },
         error_diagnostic_selected = {
-          bg = 'none'
+          bg = 'none',
+        },
+        modified = {
+          bg = 'none',
+        },
+        modified_visible = {
+          bg = 'none',
         },
         modified_selected = {
-          bg = 'none'
+          bg = 'none',
         },
         duplicate_selected = {
-          bg = 'none'
+          bg = 'none',
+        },
+        duplicate_visible = {
+          bg = 'none',
+        },
+        duplicate = {
+          bg = 'none',
         },
         separator_selected = {
-          bg = 'none'
+          bg = 'none',
+        },
+        separator_visible = {
+          bg = 'none',
+        },
+        separator = {
+          bg = 'none',
+        },
+        indicator_visible = {
+          bg = 'none',
         },
         indicator_selected = {
-          bg = 'none'
+          bg = 'none',
         },
         pick_selected = {
-          bg = 'none'
+          bg = 'none',
         },
+        pick_visible = {
+          bg = 'none',
+        },
+        pick = {
+          bg = 'none',
+        },
+        offset_separator = {
+          bg = 'none',
+        },
+        trunc_marker = {
+          bg = 'none',
+        }
       }
     },
   },
