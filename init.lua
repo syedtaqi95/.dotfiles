0cd5b08a1cf3a4e92612ff8bab23252d1eb0bc37
syedtaqi95 <sh13226@my.bristol.ac.uk>
2025-07-11 12:45:47 +0100

    feat: replace swenv.nvim with linux-cultist/venv-selector.nvim

--- init.lua
+++ init.lua
@@ -880,7 +880,24 @@ require('lazy').setup({
 
   { "lukas-reineke/virt-column.nvim", opts = {} },
 
-  "AckslD/swenv.nvim",
+  -- Select python virtual environment from inside neovim
+  {
+    'linux-cultist/venv-selector.nvim',
+    branch = 'regexp',
+    dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
+    opts = {
+      -- Your options go here
+      -- name = "venv",
+      -- auto_refresh = false
+    },
+    event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+    keys = {
+      -- Keymap to open VenvSelector to pick a venv.
+      { '<leader>vs', '<cmd>VenvSelect<cr>' },
+      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+      { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+    },
+  },
 
   {
     "NoahTheDuke/vim-just",
